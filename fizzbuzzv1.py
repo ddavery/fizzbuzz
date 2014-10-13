1cbf3c4f37a1b5d070fba7b5c0d6857654f9bcee
SupaDave <ddavery@gmail.com>
2014-10-13 09:48:25 -0400

    version 3

--- fizzbuzzv1.py
+++ fizzbuzzv1.py
@@ -1,5 +1,5 @@
-numbers  = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-for num in numbers:
+#numbers  = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+for num in range(1,51):
     if num%3 == 0 and not num%5 == 0:
       print 'fizz'
     elif num%5 == 0 and not num%3 == 0:
